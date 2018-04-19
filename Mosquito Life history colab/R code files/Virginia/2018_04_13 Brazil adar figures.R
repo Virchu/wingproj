f8566a413ca60fc08d36ec1ba839d21aa70b7473
Virchu <virgchu@gmail.com>
2018-04-18 21:11:12 -0400

    new figs

--- Mosquito Life history colab/R code files/Virginia/2018_04_13 Brazil adar figures.R
+++ Mosquito Life history colab/R code files/Virginia/2018_04_13 Brazil adar figures.R
@@ -64,8 +64,19 @@ adult.sum.nosex <- adult %>% group_by(Fam_new, Biome, State, Locality, Temp_fac,
                                                                                                           mean.Lat = mean(Latitude, na.rm = TRUE)) %>% data.frame
 adult.sum.nosex$State<-factor(adult.sum.nosex$State, levels=c("Amazonas", "Rondonia", "Tocantins", "Rio de Janeiro"))
 
-
 larv.state.line<-
+  ggplot(adult.sum.nosex, aes(x=Temp_fac, y=mean.sLL, group=interaction(Biome,Lat_grouple),colour=State, shape=Biome, linetype = Lat_grouple))+
+  geom_line(aes(linetype = Lat_grouple),size=1,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_errorbar(aes(ymin=mean.sLL-se.sLL, ymax=mean.sLL+se.sLL),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
+  geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  ggtitle("Average larvae development by temperature and state")+
+  scale_y_continuous(breaks = c(13: 24))+ theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
+  scale_x_discrete(limit = c(20, 24,28))+
+  labs(x="Temperature (C)", y="Larvae development (days)", linetype="Latitude group" )
+
+larv.state.line1<-
   ggplot(adult.sum.nosex, aes(x=Temp_fac, y=mean.sLL, group=State,colour=State))+
   stat_summary(fun.y=mean, 
                fun.ymin = function(x) mean(x) - sd(x), 
@@ -93,4 +104,29 @@ larv.locality.line<-
   xlab("Temperature (C)")+ ylab("Larvae time (days)") +
   scale_y_continuous(breaks = c(13: 24))+ theme_classic() +theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))
                                                                                                      
+#adult life
+adult.state.line<-
+  ggplot(adult.sum.nosex, aes(x=Temp_fac, y=mean.AL, group=interaction(Biome,Lat_grouple),colour=State, shape=Biome, linetype = Lat_grouple))+
+  geom_line(aes(linetype = Lat_grouple),size=1,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_errorbar(aes(ymin=mean.AL-se.AL, ymax=mean.AL+se.AL),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
+  geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  ggtitle("Average adult life by temperature and state")+
+  scale_y_continuous(breaks = c(1: 6))+ theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
+  scale_x_discrete(limit = c(20, 24,28))+
+  labs(x="Temperature (C)", y="Adult time (days)", linetype="Latitude group" )
+
+#wing length
+wing.state.line<-
+  ggplot(adult.sum.nosex, aes(x=Temp_fac, y=mean.wing, group=interaction(Biome,Lat_grouple),colour=State, shape=Biome, linetype = Lat_grouple))+
+  geom_line(aes(linetype = Lat_grouple),size=1,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_errorbar(aes(ymin=mean.wing-se.wing, ymax=mean.wing+se.wing),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
+  geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  ggtitle("Average wing length by temperature and state")+
+  #scale_y_continuous(breaks = c(1: 6))+ theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
+  scale_x_discrete(limit = c(20, 24,28))+
+  labs(x="Temperature (C)", y="Adult time (days)", linetype="Latitude group" )
 
