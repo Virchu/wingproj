4ae7331ea6e6ae34a73c1e65e3cf536520cee27f
Virchu <virgchu@gmail.com>
2018-04-21 17:05:02 -0400

    Trying bayes models

--- Mosquito Life history colab/R code files/Virginia/2018_04_19 Brazil adar bayes models.R
+++ Mosquito Life history colab/R code files/Virginia/2018_04_19 Brazil adar bayes models.R
@@ -1,6 +1,13 @@
 library(Rcpp)
 library(brms)
-
+library(dplyr)
+library(ggplot2)
+library(lme4)
+library(raster)
+library(cowplot)
+library(car)
+library(rstan)
+#library(shinystan)
 #read in and check data
 #adult <- read.csv("C:\\Users\\Tim\\Desktop\\mosquito\\2018_03_07 Life history adult data.csv")
 all_data <- read.csv("C:\\Users\\virgc\\GitHub\\wingproj\\Mosquito Life history colab\\Data files\\2018_03_28 Brazil adar16 Life history full.csv")
@@ -74,5 +81,25 @@ brm1 <- brm(mean.sLL~ 1+ Temp_fac + State, data = adult.sum.nosex,
                        prior(normal(0, 1), "Intercept"),
                        prior(normal(0, 1), "sigma")))
 
+# model checking
+summary(brm1)
+fixef(brm1)
+bayes_R2(brm1) # Bayesian R2 estimate, get confidence interval
+
+# use shiny to take a look at model outputs, diagnostic plots
+launch_shiny(brm1)
+
+
 #multivariate model to look at correlation
-multi_adult<- brm(cbind(mean.sLL, mean.AL, mean.wing)~ Temp_fac+State+(1|m|Fam_new), data=adult.sum.nosex, chains=2, cones=2)
+get_prior(cbind(mean.sLL, mean.AL, mean.wing)~ Temp_fac+State+(1|m|Fam_new), data=adult.sum.nosex)
+
+multi_adult<- brm(cbind(mean.sLL, mean.AL, mean.wing)~ 1+Temp_fac+State+(1|m|Fam_new), data=adult.sum.nosex, 
+                  chains=2, cones=2,control= list(adapt_delta = 0.9),
+                  iter = 3000, warmup = 1500, thin = 5, 
+                  prior =  c(prior(normal(0, 1), "b"),
+                             prior(normal(0, 1), "cor"),
+                             prior(normal(0, 1), "Intercept"),
+                             prior(normal(0, 1), "sigma"),
+                             prior(normal(0, 1), "sd"),
+                             prior(normal(0, 1), "rescor")
+                             ))
