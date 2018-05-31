2c1c8a8d9a020a80e4b2d6daa9d6c4c554924507
Virchu <virgchu@gmail.com>
2018-05-31 17:29:30 -0400

    Working on results for LH regional
    glmmpql broken with lsmeans? trying to get stargazer to work but doesn't like class of glmmpql

--- Mosquito Life history colab/R code files/Virginia/2018_05_23 Brazil adar results draft code.R
+++ Mosquito Life history colab/R code files/Virginia/2018_05_23 Brazil adar results draft code.R
@@ -12,9 +12,12 @@ library(GGally)
 library(ggpubr)
 library(survival)
 library(survminer)
+library(stargazer)
 ##Datafiles
 #complete data set
 all_data <- read.csv("C:\\Users\\vmc04\\Documents\\GitHub\\wingproj\\Mosquito Life history colab\\Data files\\2018_03_28 Brazil adar16 Life history full.csv")
+#add survival prob
+#all_data$surv<- 1-(1/all_data$AL)
 #create dataframe of survived to adult data
 adult<-subset(all_data, all_data$Death_stat==1 & !is.na(all_data$Sex1))
 #field wing data
@@ -26,6 +29,7 @@ all_data$Locality<-factor(all_data$Locality, levels=c("ARS", "APR", "RPV", "RMO"
 adult$State<-factor(adult$State, levels=c("Amazonas", "Rondonia", "Tocantins", "Rio de Janeiro"))
 adult$Locality<-factor(adult$Locality, levels=c("ARS", "APR", "RPV", "RMO","TLC","TPN", "SJU"))
 adult$Temp_fac<-factor(adult$Temp_num, levels=c(20,24,28))
+#adult$surv<-replace(adult$surv, is.infinite(adult$surv), NA)
 
 ##Organize data- family averages, some include sex
 #all_data (n=3430) with separate sexes
@@ -36,11 +40,26 @@ all.sum <- all_data %>% group_by(Fam_new, Biome, State, Locality, Sex, Lat_group
                                                                                                 mean.surv= mean(Death_stat, na.rm=TRUE),
                                                                                                 mean.Temp= mean(Temp_num, na.rm=TRUE),
                                                                                                 mean.Lat=mean(Latitude, na.rm=TRUE)) %>% data.frame
+                                                                                                
 #all_data (n=3430) grouping sexes
-all.sum.nosex <- all_data %>% group_by(Fam_new, Biome, State, Locality, Lat_group) %>% summarize(mean.sLL = mean(sLL, na.rm = TRUE),
+all.sum.nosex <- all_data %>% group_by(Fam_new, Biome, State, Locality, Lat_group, Temp_num) %>% summarize(mean.sLL = mean(sLL, na.rm = TRUE),
+                                                                                                 N.sLL=length(sLL),
+                                                                                                 sd.sLL= sd(sLL, na.rm=TRUE),
                                                                                                  mean.surv= mean(Death_stat, na.rm=TRUE),
                                                                                                  mean.Temp = mean(Temp_num, na.rm=TRUE),
-                                                                                                 mean.Lat=mean(Latitude, na.rm=TRUE)) %>% data.frame
+                                                                                                 mean.Lat=mean(Latitude, na.rm=TRUE),
+                                                                                                 se.sLL=sd.sLL/sqrt(N.sLL),
+                                                                                                 mean.AL = mean(AL, na.rm=TRUE),
+                                                                                                 sd.AL= sd(AL, na.rm=TRUE),
+                                                                                                 N.AL=length(AL),
+                                                                                                 se.AL=sd.AL/sqrt(N.AL),
+                                                                                                 mean.wing = mean(Wing.length..mm., na.rm=TRUE),
+                                                                                                 sd.wing= sd(Wing.length..mm., na.rm=TRUE),
+                                                                                                 N.wing=length(Wing.length..mm.),
+                                                                                                 se.wing=sd.wing/sqrt(N.wing),
+                                                                                                 sum.AL=sum(AL, na.rm=TRUE),
+                                                                                                 surv.fam=1-(N.sLL/sum.AL)) %>% data.frame
+                                                                                                 
 
 #adult (n=2652) with separate sexes
 adult.sum <- adult %>% group_by(Fam_new, Biome, State, Locality, Sex, Temp_fac, Lat_group) %>% summarize(mean.sLL = mean(sLL, na.rm = TRUE), 
@@ -55,8 +74,10 @@ adult.sum <- adult %>% group_by(Fam_new, Biome, State, Locality, Sex, Temp_fac, 
                                                                                                            sd.wing= sd(Wing.length..mm., na.rm=TRUE),
                                                                                                            N.wing=length(Wing.length..mm.),
                                                                                                            se.wing=sd.wing/sqrt(N.wing),
-                                                                                                           mean.Temp = mean(Temp_num), na.rm = TRUE, 
-                                                                                                           mean.Lat = mean(Latitude, na.rm = TRUE)) %>% data.frame
+                                                                                                           mean.Temp = mean(Temp_num, na.rm = TRUE), 
+                                                                                                           mean.Lat = mean(Latitude, na.rm = TRUE),
+                                                                                                           sum.AL=sum(AL, na.rm=TRUE),
+                                                                                                           surv.fam=1-(N.sLL/sum.AL)) %>% data.frame
 
 #adult (n=2652)grouping sexes
 adult.sum.nosex <- adult %>% group_by(Fam_new, Biome, State, Locality, Temp_fac, Lat_group) %>% summarize(mean.sLL = mean(sLL, na.rm = TRUE),
@@ -72,7 +93,9 @@ adult.sum.nosex <- adult %>% group_by(Fam_new, Biome, State, Locality, Temp_fac,
                                                                                                             N.wing=length(Wing.length..mm.),
                                                                                                             se.wing=sd.wing/sqrt(N.wing),
                                                                                                             mean.Temp = mean(Temp_num), na.rm = TRUE, 
-                                                                                                            mean.Lat = mean(Latitude, na.rm = TRUE)) %>% data.frame
+                                                                                                            mean.Lat = mean(Latitude, na.rm = TRUE),
+                                                                                                            sum.AL=sum(AL, na.rm=TRUE),
+                                                                                                            surv.fam=1-(N.sLL/sum.AL)) %>% data.frame
 adult.sum.nosex$State<-factor(adult.sum.nosex$State, levels=c("Amazonas", "Rondonia", "Tocantins", "Rio de Janeiro"))
 adult.sum.nosex$Lat_group<-factor(adult.sum.nosex$Lat_group, levels=c(1,2,3))
 
@@ -84,7 +107,7 @@ field.sum <- field_wing %>% group_by(Biome, State, Latitude, Lat_grouple) %>%
 field.sum$State<-factor(field.sum$State, levels=c("Amazonas", "Rondonia", "Tocantins", "Rio de Janeiro"))
 
 #group for correlations
-adult.sum.corsub <- adult.sum[c(1:8,12,16)]
+adult.sum.corsub <- adult.sum[c(1:8,12,16,23)]
 adult.sum.corsub$Temp<-as.numeric(adult.sum.corsub$Temp_fac)
 adult.sum.corsub$Sex0<-ifelse(adult.sum.corsub$Sex=="M", 1,0)
 adult.sum.corsub$Bio<-ifelse(adult.sum.corsub$Biome=="Amazon", 0,
@@ -134,7 +157,7 @@ larv.state.line<-
   geom_errorbar(aes(ymin=mean.sLL-se.sLL, ymax=mean.sLL+se.sLL),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
   geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
   geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
-  ggtitle("Average larvae development by temperature and state")+
+  ggtitle("Average larvae development (days)")+
   scale_y_continuous(breaks = c(13: 24))+ theme_classic() +
   theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
   labs(x="Temperature (C)", y="Larvae development (days)", linetype="Latitude group" )
@@ -180,10 +203,9 @@ adult.state.line<-
   geom_errorbar(aes(ymin=mean.AL-se.AL, ymax=mean.AL+se.AL),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
   geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
   geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
-  ggtitle("Average adult life by temperature and state")+
+  ggtitle("Average adult life (days)")+
   scale_y_continuous(breaks = c(1: 6))+ theme_classic() +
   theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
-  scale_x_discrete(limit = c(20, 24,28))+
   labs(x="Temperature (C)", y="Adult time (days)", linetype="Latitude group" )
 
 #Wing length#
@@ -215,10 +237,22 @@ wing.rj <-
   ggtitle("Rio de Janeiro State")+
   xlab("Temperature (C)")+ ylab("Wing length (mm)") +
   scale_y_continuous(breaks = c(0:6))+ theme_classic() +theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=7))
-
+##prob survival
+surv.state.line<-
+  ggplot(adult.sum.nosex, aes(x=Temp_fac, y=surv.fam, group=interaction(Biome,Lat_group),colour=State, shape=Biome, linetype = Lat_group))+
+  geom_line(aes(linetype = Lat_group),size=1,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  #geom_errorbar(aes(ymin=mean.surv-se.surv, ymax=mean.surv+se.surv),stat="summary", colour="black",linetype=1,size=.8,width=.6, position=position_dodge(0.5))+
+  geom_point(colour="black", size=4,position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  geom_point(aes(shape = Biome), size=2.1, position=position_dodge(0.5), stat="summary", fun.y="mean")+
+  ggtitle("Average probability of survival")+
+  theme_classic() + 
+  theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=10))+
+  labs(x="Temperature (C)", y="Probability of survival", linetype="Latitude group" )
 #arrange plots
 grid.arrange(wing.am, wing.ro, wing.to, wing.rj, ncol=2, nrow=2, top="Average Wing length (mm) by Temperature and Family")
 
+###Arrange line graphs for figure 1
+ggarrange(larv.state.line, adult.state.line, ncol=1, nrow=2,  common.legend = TRUE, legend="bottom")
 
 ##GLMM models
 #redid with state instead of pop for SPH poster
@@ -228,68 +262,79 @@ summary(PQL_meth_sLL)
 lar_ls<- lsmeans(PQL_meth_sLL, pairwise ~Temp_fac:State, adjust="tukey")
 lar_cld<- cld(lar_ls, alpha=0.5, Letters=letters, adjust="tukey")
 
-PQL_meth_em<- glmmPQL(Emtime~Temp_fac*Pop, ~1|Locality/Family, family=gaussian(link="log"), data=adult, verbose=FALSE)
-summary(PQL_meth_em)
-#comparing interactions
-em_ls<- lsmeans(PQL_meth_em, pairwise ~Temp_fac:Pop, adjust="tukey")
-em_cld<- cld(em_ls, alpha=0.5, Letters=letters, adjust="tukey")
 
-PQL_meth_al<- glmmPQL(AL~Temp_fac*State, ~1|Locality/Family, family=poisson, data=adar_adult, verbose=FALSE)
+glm_sLL2<-glmer(sLL~Temp_fac*State+(1|Family), family=gaussian(link="log"), data=adult)
+summary(glht(glm_sLL2, mcp(Temp_fac="Tukey")))
+summary(glht(glm_sLL2, mcp(Temp_fac="Tukey", State="Tukey")))
+
+#pretty table for sLL
+stargazer(PQL_meth_sLL,  title= "GLMM results for temperature and state on larvae development", digits=2, align=TRUE)
+
+
+PQL_meth_al<- glmmPQL(AL~Temp_fac*State, ~1|Locality/Family, family=poisson, data=adult, verbose=FALSE)
 summary(PQL_meth_al)
 #comparing interactions
 al_ls<- lsmeans(PQL_meth_al, pairwise ~Temp_fac:State, adjust="tukey")
 al_cld<- cld(al_ls, alpha=0.5, Letters=letters, adjust="tukey")
 
-PQL_meth_wing<- glmmPQL(Wing.length..mm.~Temp_fac*State, ~1|Locality/Family, family=gaussian(link="log"), data=adar_adult, verbose=FALSE)
+PQL_meth_wing<- glmmPQL(Wing.length..mm.~Temp_fac*State, ~1|Locality/Family, family=gaussian(link="log"), data=adult, verbose=FALSE)
 summary(PQL_meth_wing)
 #comparing interactions
 wing_ls<- lsmeans(PQL_meth_wing, pairwise ~Temp_fac:State, adjust="tukey")
 wing_cld<- cld(wing_ls, alpha=0.5, Letters=letters, adjust="tukey")
 
-##ANOVA tables for gen var, phen plast and G by E
+##ANOVA tables for gen var, phen plast and G by E of traits
 #Larvae
-larv_mod<-lmer(sLL~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+larv_mod<-lmer(sLL~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(larv_mod) #both Treatment and State sig as well as interaction
   difflsmeans(larv_mod, test.effs="State") #all state comparisons except rio and tocantins significant
 difflsmeans(larv_mod, test.effs="Temp_let") #all temp comparisons significant
 l.vc<-VarCorr(larv_mod)
 print(l.vc, comp=c("Variance"))
 #adult
-adult_mod<-lmer(AL~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+adult_mod<-lmer(AL~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(adult_mod) #both Treatment and State sig as well as interaction
 rand(adult_mod)#get random effects table
 difflsmeans(adult_mod, test.effs="State") #all state comparisons except rio and tocantins significant
 difflsmeans(adult_mod, test.effs="Temp_let") #all temp comparisons significant
 
 #wing length
-wing_mod<-lmer(Wing.length..mm.~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+wing_mod<-lmer(Wing.length..mm.~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(wing_mod) #both Treatment and State sig as well as interaction
 rand(wing_mod)#get random effects table
 difflsmeans(wing_mod, test.effs="State") #Wing.length..mm.all state comparisons except amazonas and rondonia significant
 difflsmeans(wing_mod, test.effs="Temp_let") #Wing.length..mm.all temp comparisons significant
 
+surv_mod<-lmer(surv~Temp_let*State+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
+anova(surv_mod) #both Treatment and State sig as well as interaction
+rand(surv_mod)#get random effects table
+difflsmeans(surv_mod, test.effs="State") #Wing.length..mm.all state comparisons except amazonas and rondonia significant
+difflsmeans(surv_mod, test.effs="Temp_let") #Wing.length..mm.all temp comparisons significant
+###ANOVAs by population for comparison
+
 ###ANOVAS with sex for supplemental
 #male=1
-larv_mod1<-lmer(sLL~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+larv_mod1<-lmer(sLL~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(larv_mod1) #both Treatment and State sig as well as interaction
 
-adult_mod1<-lmer(AL~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+adult_mod1<-lmer(AL~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(adult_mod1)
 
-wing_mod1<-lmer(Wing.length..mm.~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adar_adult)
+wing_mod1<-lmer(Wing.length..mm.~Temp_let*State*Sex1+(1|Fam_new)+(1|State:Temp_let:Fam_new), data=adult)
 anova(wing_mod1)
 
 
 ### Correlations: 
-adult.sum.corsub1<-adult.sum.corsub[c(1,13,7,14,4,11,12,8:10)]
-names(adult.sum.corsub1)<-c("Family", "Biome", "Latitude", "State", "Locality","Temp", "Sex", "Avg.Larvae", "Avg.Adult", "Avg.Wing")
+#added avg. surv (11), debating removing it...
+adult.sum.corsub1<-adult.sum.corsub[c(1,14,7,15,4,12,13,8:11)]
+names(adult.sum.corsub1)<-c("Family", "Biome", "Latitude", "State", "Locality","Temp", "Sex", "Avg.Larvae", "Avg.Adult", "Avg.Wing", "Avg.Surv")
 ggcorr(adult.sum.corsub1,
        label=TRUE,
        label_alpha=0.75,
        hjust=0.4)
 
 #smaller graph
-cor1<-ggcorr(adult.sum.corsub1[,c(4,6:10)],
+cor1<-ggcorr(adult.sum.corsub1[,c(4,6:11)],
        label=TRUE,
        label_alpha=0.75,
        hjust=0.65,
@@ -375,12 +420,29 @@ ggsurv<-ggsurvplot(survival.fit1,  conf.int=TRUE, color="Temp_fac", palette=c("#
                    font.main= c(20, "bold"),
                    font.x= 15,
                    font.y=15,
-                   risk.table = )
+                   risk.table = "abs_pct")
 ggsurv$plot +theme_bw() + facet_grid (State~.) +theme(plot.title = element_text(hjust = 0.5))
-
+#risk.table
+survtab<-ggsurv$data.survplot
+cumsurvtab<-ggsurv$data.survtable
 #fit cox model
 cox<- coxph(Surv(time)~Temp_fac+State, data=adult)
 summary(cox)
 
 cox1<- coxph(Surv(time)~Temp_num*Latitude, data=adult)
 summary(cox1)
+
+##quick plot for family diff
+adult.rj <-
+  ggplot(adult.sum.nosex, aes(x=Temp_fac, y=mean.AL, group=Fam_new))+
+  geom_line(aes(colour=Fam_new), subset(adult.sum.nosex, State=="Rio de Janeiro"))+
+  ggtitle("Rio de Janeiro State")+
+  xlab("Temperature (C)")+ ylab("Adult time (days)") +
+  scale_y_continuous(breaks = c(0:6))+ theme_classic() +theme(plot.title = element_text(hjust = 0.5), legend.text = element_text(size=7))
+
+rio_fam<-subset(all_data, State=="Rio de Janeiro")
+survival.fit2<-survfit(Surv(time)~Temp_num+Fam_new, data=rio_fam)
+#summarize
+riores_sum<- surv_summary(survival.fit2)
+ggsurv<-ggsurvplot(survival.fit2, color="Temp_num", palette=c("#619CFF", "#00BA38", "#F8766D"), conf.int=TRUE, na.rm=TRUE)
+ggsurv$plot +theme_bw() + facet_grid (Fam_new~.)
