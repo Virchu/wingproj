a8820b63dfda66b77c9e368d442e3605b6c279b7
Virchu <virgchu@gmail.com>
2019-02-23 15:12:48 -0500

    Update Nydar local metap on GEI values.R

--- Mosquito Life history colab/R code files/Virginia/Nydar local metap on GEI values.R
+++ Mosquito Life history colab/R code files/Virginia/Nydar local metap on GEI values.R
@@ -14,10 +14,18 @@ adult<-c(.019,.000000000656, .209,.708,.85,.8287,.246)
 wing<-c(.0816,.07758,.0624,.035,.306,.0934,.0205)
 GEI.data<-data.frame(locality,larv,adult,wing)
 
-#looking at Fischers result (sumlog)
+#looking at Fishers result (sumlog)
 allmetap(GEI.data$larv, method="all")
 #sumlog=0.00031417
 allmetap(GEI.data$adult, method="all")
 #sumlog=3.14e-7
 allmetap(GEI.data$wing, method="all")
 #sumlog=0.00067311
+
+plotp(GEI.data$larv)
+plotp(GEI.data$adult)
+plotp(GEI.data$wing)
+
+schweder(GEI.data$larv)
+schweder(GEI.data$adult)
+schweder(GEI.data$wing)
